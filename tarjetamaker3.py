0d014547c10b4804e222b3ad41702c95b1180ede
Grant Curell <grantcurell@gmail.com>
2018-03-18 11:29:49 -0500

    Delete QT Folder

--- tarjetamaker3.py
+++ tarjetamaker3.py
@@ -1,28 +1,23 @@
-
 # import the main window object (mw) from aqt
 from aqt import mw
 # import the "show info" tool from utils.py
 from aqt.utils import showInfo
 # import all of the Qt GUI library
 from aqt.qt import *
-from PyQt4 import QtCore, QtGui
-from tarjetamaker3.tarjeta_ui import Ui_tarjeta_ui
-import sys
 
 # We're going to add a menu item below. First we want to create a function to
 # be called when the menu item is activated.
 
-def start_tarjeta_maker():
-    app = QtGui.QApplication(sys.argv)
-    tarjeta_ui = QtGui.QWidget()
-    ui = Ui_tarjeta_ui()
-    ui.setupUi(tarjeta_ui)
-    tarjeta_ui.show()
-    sys.exit(app.exec_())
+def testFunction():
+    # get the number of cards in the current collection, which is stored in
+    # the main window
+    cardCount = mw.col.cardCount()
+    # show a message box
+    showInfo("Card count: %d" % cardCount)
 
 # create a new menu item, "test"
-action = QAction("Tarjeta Maker", mw)
+action = QAction("test", mw)
 # set it to call testFunction when it's clicked
-action.triggered.connect(start_tarjeta_maker)
+action.triggered.connect(testFunction)
 # and add it to the tools menu
 mw.form.menuTools.addAction(action)
--- tarjetamaker3/3to2.py
+++
@@ -1,6 +0,0 @@
-#!c:\users\foozand\appdata\local\programs\python\python36-32\python.exe
-import sys
-
-from lib3to2.main import main
-
-sys.exit(main("lib3to2.fixes"))
